96dc56c99fd0c41b900f94d7e7ef3325df21c684
crystaltian97 <56941119+crystaltian97@users.noreply.github.com>
2020-02-10 20:38:06 -0500

    Add files via upload

--- class_example.py
+++ class_example.py
@@ -1,2 +1,19 @@
-def do_sum(first_arg, sec_arg, thrid_arg):
-	return first_arg+sec_arg+third_arg
+import unittest
+import class_example
+
+class TestMyModule(unittest.TestCase):
+	
+	def setUp(self):
+		return 
+
+
+	def do_divide(first_arg, sec_arg):
+
+		first_arg = 4
+		sec_arg = 2
+
+		result = class_example.do_divide(first_arg,sec_arg)
+		expected_result = 2
+
+		self.assertEqual(result, expected_result)
+
