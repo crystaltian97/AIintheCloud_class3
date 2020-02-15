447312a8598986a7d5ccfe3e28c171b923e798b5
crystal <xt2230@columbia.edu>
2020-02-15 13:42:39 -0500

    modified class_example and test example

--- class_example.py
+++ class_example.py
@@ -1,19 +1,3 @@
-import unittest
-import class_example
-
-class TestMyModule(unittest.TestCase):
+def do_divide(arg_1, arg_2):
 	
-	def setUp(self):
-		return 
-
-
-	def do_divide(first_arg, sec_arg):
-
-		first_arg = 4
-		sec_arg = 2
-
-		result = class_example.do_divide(first_arg,sec_arg)
-		expected_result = 2
-
-		self.assertEqual(result, expected_result)
-
+	return arg_1 / arg_2
--- class_example_test.py
+++ class_example_test.py
@@ -2,18 +2,17 @@ import unittest
 import class_example
 
 class TestMyModule(unittest.TestCase):
-	
+
 	def setUp(self):
-		return 
+		return
 
-
-	def do_divide(first_arg, sec_arg):
+	def test_do_divide(self):
 
 		first_arg = 4
-		sec_arg = 2
+		second_arg = 2
 
-		result = class_example.do_divide(first_arg,sec_arg)
+		result = class_example.do_divide(first_arg, second_arg)
+
 		expected_result = 2
 
 		self.assertEqual(result, expected_result)
-
